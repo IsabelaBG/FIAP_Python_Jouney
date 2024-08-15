6c88237808b958d7125f8bf5695974d8e6f06498
unknown <isabelabianchigomes@gmail.com>
2024-08-14 22:08:45 -0300

    atualizacao aula 4

--- Aula_4/03_NdArray.py
+++ Aula_4/03_NdArray.py
@@ -5,3 +5,9 @@ lista = [1,2,3]
 print(lista)
 novo_array = np.array(lista)
 print(novo_array)
+
+print("Esse é um ndarray com {} dimensões".format(novo_array.ndim))
+print("As dimensões desse ndarray possuem os seguintes tamanhos: {}".format(novo_array.shape))
+print("Ao todo, esse ndarray contém {} elementos".format(novo_array.size))
+print("Os elementos presentes nesse ndarray são do tipo: {}".format(novo_array.dtype))
+print("Os elementos presentes nesse ndarray ocupam: {} bytes".format(novo_array.itemsize))
