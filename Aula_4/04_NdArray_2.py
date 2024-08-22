9aa613bf517c0e0e150cc7374e98db963cab57c9
unknown <isabelabianchigomes@gmail.com>
2024-08-21 21:54:07 -0300

    atualizacao Aula 4

---
+++ Aula_4/04_NdArray_2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array_tupla = np.array([(1,2,3),(4,5,6)])
+print("Abaixo está o array formado através da tupla:")
+print(array_tupla)
+
+array_lista = np.array([[1,2,3],[4,5,6]])
+print("Abaixo está o array formado através de listas:")
+print(array_lista)
---
+++ Aula_4/05_NdArray_3.py
@@ -0,0 +1,18 @@
+import numpy as np
+#funções numpy para popular array, matrizes. ao indicar o ntype limita-se para inteiros
+
+array_zerado = np.zeros((3,4))
+print("O array gerado com 0 ficou assim: ")
+print(array_zerado)
+
+array_com_um = np.ones((5,2),dtype=np.int32)
+print("O array gerado com 1 ficou assim: ")
+print(array_com_um)
+
+array_aleatorio = np.empty((6,3),dtype=np.int64)
+print("O array gerado com valores aleatórios ficou assim: ")
+print(array_aleatorio)
+
+array_gerado = np.arange(10)
+print("O array gerado ficou assim: ")
+print(array_gerado)
